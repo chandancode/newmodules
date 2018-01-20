b5f79b47b24f7c412d406c622609388e6de847e1
chandan <chandan.cse06217@gmail.com>
2018-01-20 12:54:59 -0500

    new

--- keys/keys.tf
+++ keys/keys.tf
@@ -1,5 +1,6 @@
 resource "aws_key_pair" "chandankey" {
   key_name   = "chandankey"
-  public_key = "${file("${path.module}/${var.PATH_TO_PUBLIC_KEY}")}"
+  public_key = "${file("${var.PATH_TO_PUBLIC_KEY}")}"
+  directory = "/KEYS"
 }
 
